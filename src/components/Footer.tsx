87a213d64e1e7ae672188c4f43d3d815601e5107
aarmaanpahuja-sudo <aarmaan.pahuja@gmail.com>
2026-07-16 16:00:46 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
         <div className="flex items-center justify-center gap-4">
 
           <p className="whitespace-nowrap">
-            © {new Date().getFullYear()} SafeLoudoun
+            © {new Date().getFullYear()} VexGrid
           </p>
 
           {/* Social Icons */}
@@ -43,7 +43,7 @@ export default function Footer() {
 
             {/* GitHub */}
             <a
-              href="https://github.com/aarmaanpahuja-sudo/SafeLoudoun"
+              href="https://github.com/aarmaanpahuja-sudo/VexGrid"
               target="_blank"
               rel="noopener noreferrer"
               aria-label="GitHub"
